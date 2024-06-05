58df5a218703e014784c23957c3824ce0adb3377
frog.com <90309791+frogcom@users.noreply.github.com>
2024-06-05 22:16:07 +0200

    fix

--- database/migrations/2024_06_05_142526_create_modules_table.php
+++ database/migrations/2024_06_05_142526_create_modules_table.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Models\category;
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -14,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(category::class)->nullable();
+            $table->foreignIdFor(Category::class)->nullable();
             $table->string('title');
             $table->longText('text');
             $table->integer('available_spots');
