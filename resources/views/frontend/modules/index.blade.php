e4715b5562590f751f35156e6a6dbac05aa9c207
frog.com <90309791+frogcom@users.noreply.github.com>
2024-06-06 20:49:35 +0200

    fix

--- resources/views/frontend/modules/index.blade.php
+++ resources/views/frontend/modules/index.blade.php
@@ -3,7 +3,7 @@
 <div class=" max-w-screen-xl mx-auto">
 
     <div class="text-center">
-        <h2 class="text-5xl my-8 lg:text-7xl">keuzemodules</h2>
+        <h2 class="text-5xl my-8 lg:text-7xl break-after-auto">keuzemodules</h2>
         <p class="mb-16">Hier een overzicht van alle keuzemodules die deze periode worden aangeboden</p>
     </div>
 
@@ -14,7 +14,6 @@
         <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 ">
             @foreach($modules as $module)
                 <x-module.card :module="$module"/>
-
             @endforeach
         </div>
 
