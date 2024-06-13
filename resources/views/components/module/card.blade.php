de0cc5c9f1b68ef2ae140dc972ee1ecf8e6992b7
frog.com <90309791+frogcom@users.noreply.github.com>
2024-06-13 13:18:40 +0200

    fix

--- resources/views/components/module/card.blade.php
+++ resources/views/components/module/card.blade.php
@@ -1,7 +1,7 @@
 <a href="{{route('keuzemodules.show',$module->id)}}">
     <div class="flex align-sub h-[36rem] w-auto bg-white bg-cover bg-center p-6 max-w-[112rem]" style="background-image: url('{{asset('storage/'. $module->image->url)}}');">
         <div class="self-end">
-            <div class=" bg-white text-base-content max-w-full px-4 text-center my-1 rounded-full">
+            <div class=" bg-white text-base-content w-full px-4 text-center my-1 rounded-full">
                 {{$module->category->name}}
             </div>
             <h5 class="mb-2 text-4xl leading-tight  text-white dark:text-neutral-50">
